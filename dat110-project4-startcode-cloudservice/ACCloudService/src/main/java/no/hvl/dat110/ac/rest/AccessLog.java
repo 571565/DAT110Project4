4d1134fa98b6fd9de8516d8bd4337801b3b8ff4a
571565 <571565@stud.hvl.no>
2019-05-02 14:40:13 +0200

    Ferdig

---
+++ dat110-project4-startcode-cloudservice/ACCloudService/src/main/java/no/hvl/dat110/ac/rest/AccessLog.java
@@ -0,0 +1,47 @@
+package no.hvl.dat110.ac.rest;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.gson.Gson;
+
+public class AccessLog {
+	
+	private AtomicInteger cid;
+	protected ConcurrentHashMap<Integer, AccessEntry> log;
+	
+	public AccessLog () {
+		this.log = new ConcurrentHashMap<Integer,AccessEntry>();
+		cid = new AtomicInteger(0);
+	}
+
+	// TODO: add an access entry for the message and return assigned id
+	public int add(String message) {
+		int id = cid.getAndIncrement();
+		AccessEntry accessEntry = new AccessEntry(id, message);
+		log.put(id, accessEntry);
+		return id;
+	}
+		
+	// TODO: retrieve a specific access entry 
+	public AccessEntry get(int id) {
+		
+		return log.get(id);
+		
+	}
+	
+	// TODO: clear the access entry log
+	public void clear() {
+		log.clear();
+	}
+	
+	// TODO: JSON representation of the access log
+	public String toJson () {
+		
+		
+    	
+		Gson gson = new Gson();
+
+    	return gson.toJson(log.values().toArray());
+    }
+}
---
+++ dat110-project4-startcode-cloudservice/ACCloudService/src/main/java/no/hvl/dat110/ac/rest/App.java
@@ -0,0 +1,92 @@
+package no.hvl.dat110.ac.rest;
+
+import static spark.Spark.after;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.put;
+import static spark.Spark.post;
+import static spark.Spark.delete;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	static AccessLog accesslog = null;
+	static AccessCode accesscode = null;
+	
+	public static void main(String[] args) {
+
+		if (args.length > 0) {
+			port(Integer.parseInt(args[0]));
+		} else {
+			port(8080);
+		}
+
+		// objects for data stored in the service
+		
+		accesslog = new AccessLog();
+		accesscode  = new AccessCode();
+		
+		after((req, res) -> {
+  		  res.type("application/json");
+  		});
+		
+		// for basic testing purposes
+		get("/accessdevice/hello", (req, res) -> {
+			
+		 	Gson gson = new Gson();
+		 	
+		 	return gson.toJson("IoT Access Control Device");
+		});
+		
+		// TODO: implement the routes required for the access control service
+		
+		
+		post("/accessdevice/log", (request, response) -> {
+			Gson gson= new Gson();
+			AccessMessage am = gson.fromJson(request.body(),AccessMessage.class );
+			int id = accesslog.add(am.getMessage());
+			return gson.toJson(accesslog.get(id));
+		});
+
+		get("/accessdevice/log", (req, res) -> {
+			return accesslog.toJson();
+		});
+
+		get("/accessdevice/log/:id", (req, res) -> {
+			int id = -1;
+			try {
+				id = Integer.parseInt(req.params(":id"));
+
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+
+			Gson gson = new Gson();
+			return gson.toJson(accesslog.get(id));
+		});
+
+		put("accessdevice/code", (req,res) -> {
+			Gson gson = new Gson();
+			AccessCode ac = gson.fromJson(req.body(), AccessCode.class);
+			accesscode.setAccesscode(ac.getAccesscode());
+			return req.body();
+		});
+
+		get("/accessdevice/code", (request, response) -> {
+			Gson gson = new Gson();
+			return gson.toJson(accesscode);
+		});
+
+		delete("/accessdevice/log", (request, response) -> {
+			accesslog.clear();
+			return accesslog.toJson();
+		});
+		
+    }
+    
+}
